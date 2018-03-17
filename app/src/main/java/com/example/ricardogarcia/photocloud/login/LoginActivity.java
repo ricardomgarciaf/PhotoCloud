d0ebaecd6c632e694b6a72a115282937a56b7572
Ricardo Garcia <ric.garciaf@gmail.com>
2018-03-17 17:06:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ricardogarcia/photocloud/login/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.ricardogarcia.photocloud.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ricardogarcia.photocloud.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    /*
+    * ButterKnife
+    * Retrofit
+    * Retrolambda
+    * Reactive
+    *
+    * */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
